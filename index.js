ed75fb1ffb8458e500aac5fe1ca0fc088e54e779
EmpireAntz <thebigboognish@gmail.com>
2023-12-12 19:31:09 -0800

    added descriptive comments throughout the javascript.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
+//Establish connection to the mysql database
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '2L#RJWFposb66p)UL&w9',
     database: 'employee_db',
 })
-
+//Main function that initiates the application
 const main = () => {
+    //Prompts the user with choices for the CMS
     inquirer.prompt([
         {
             type: 'list',
@@ -25,30 +27,37 @@ const main = () => {
                 'Add Department'
             ]
         }
-
+    
     ]).then(answers => {
+        //Process user's choices and invokes corresponding functionality
         const choice = answers.action
-
+        //Handles the logic for view all employees
         if (choice === "View All Employees") {
+            //SQL query to get all data from employee table
             connection.query('SELECT * FROM employee', (err, results) => {
+                //Error handling
                 if (err) {
                     console.error(err)
                     return
                 }
+                //Shows the table results for employees in the console
                 console.table(results)
+                //Returns to the main function after the results are displayed
                 main()
             })
         }
-
+        //Handles the logic to add employees to the database 
         if (choice === "Add Employees") {
-            // First, fetch all roles from the database
+        //Gets all of roles from the database to display as choices
             connection.query('SELECT id, title FROM role', (err, roles) => {
+                //Error handling
                 if (err) {
-                    console.error(err);
+                    console.error(err)
                     return
                 }
-        
+                //Prompts the user to input the new employee details
                 inquirer.prompt([
+
                     { 
                         type: 'input', 
                         name: 'firstName',
@@ -65,31 +74,39 @@ const main = () => {
                         message: 'What is the employee\'s role?',
                         choices: roles.map(role => role.title)
                     }
-                ]).then(answers => {
-                    const role = roles.find(r => r.title === answers.role)
 
+                ]).then(answers => {
+                    //Finds the role ID corresponding to the selected role
+                    const role = roles.find(r => r.title === answers.role)
+                    //Inserts new employee record into the database
                     connection.query('INSERT INTO employee SET ?', {
                         first_name: answers.firstName,
                         last_name: answers.lastName,
                         role_id: role.id
                     }, (err, results) => {
+                        //Error handling
                         if (err) {
                             console.error(err)
                             return
                         }
+                        //Lets you know the employee has been added
                         console.log('Employee Added!')
+                        //Returns to the main function after the emplyee is added
                         main()
                     })
                 })
             })
         }
-
+        //Handles the logic for updating employee roles
         if (choice === "Update Employee Role") {
+             //Gets all employees to display as choices for selection
             connection.query('SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee', (err, employees) => {
+                //Error handling
                 if (err) {
-                    console.error(err);
+                    console.error(err)
                     return
                 }
+                //Prompts user to select the employee to update
                 inquirer.prompt([
                     {
                         type: 'list',
@@ -99,12 +116,14 @@ const main = () => {
                     }
                 ]).then(answers => {
                     const selectedEmployeeId = answers.employee
+                    //Gets all roles to display as choices for the new role
                     connection.query('SELECT id, title FROM role', (err, roles) => {
+                        //Error Handling
                         if (err) {
                             console.error(err)
                             return
                         }
-                    
+                         //Prompts the user to then select the new role for the selected employee
                         inquirer.prompt([
                             {
                                 type: 'list',
@@ -114,38 +133,47 @@ const main = () => {
                             }
                         ]).then(roleAnswer => {
                             const newRoleId = roleAnswer.role
+                            //Updates the employee's role in the database
                             connection.query('UPDATE employee SET role_id = ? WHERE id = ?', [newRoleId, selectedEmployeeId], (err, results) => {
+                                //Error handling 
                                 if (err) {
                                     console.error(err)
-                                    return; // Stop execution on error
+                                    return
                                 }
+                                //Lets you know the employee was updated successfully
                                 console.log('Employee\'s role updated!')
-                                main();
+                                //Returns to the main function after the employee is updated
+                                main()
                             })
                         })
                     })
                 })
             })
         }
-
+        //Handles the logic for viewing all roles
         if (choice === "View All Roles") {
+            //SQL query to get everything from the role table
             connection.query('SELECT * FROM role ', (err, results) => {
+                //Error handling
                 if (err) {
                     console.error(err)
                     return
                 }
+                //Displays the table results in the console
                 console.table(results)
+                //Returns to the main function after roles are displayed
                 main()
             })
         }
-
+        //Handles the logic for adding roles
         if (choice === "Add Role") {
-             // Fetch all departments
+              //Gets all departments to list them as choices for the role's department
             connection.query('SELECT id, department_name FROM department', (err, departments) => {
                 if (err) {
                     console.error(err)
                     return
                 }
+                //Prompts the user to input the new role details
                 inquirer.prompt([
                     {
                         type: 'input',
@@ -165,32 +193,41 @@ const main = () => {
                         choices: departments.map(dept => ({ name: dept.department_name, value: dept.id }))
                     }
                 ]).then(answers => {
+                    //Adds the new role to the database
                     connection.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', 
                     [answers.title, answers.salary, answers.department], 
                     (err, results) => {
+                        //Error Handling
                         if (err) {
                             console.error(err)
                             return
                         }
+                        //Tells the  user the new role was added
                         console.log('Role added!')
+                        //Returns to the main function adter the new role is added
                         main()
                     })
                 })
             })
         }
-
+        //Handles the logic for viewing all departments 
         if (choice === "View All Departments") {
+            //SQL query to get everything from the department table
             connection.query('SELECT * FROM department', (err, results) => {
+                //Error Handling
                 if (err) {
                     console.error(err)
                     return
                 }
+                //Shows the table results in the console
                 console.table(results)
+                //Returns to the main function after departments are displayed
                 main()
             })
         }
-
+        //Handles logic for adding new departments
         if (choice === "Add Department") {
+            //Prompts the user to inpur the new department 
             inquirer.prompt([
                 {
                     type: 'input',
@@ -198,19 +235,23 @@ const main = () => {
                     message: 'What is the name of the new department?'
                 }
             ]).then(answers => {
+                // Insert new department record into the database
                 connection.query('INSERT INTO department SET ?', 
                 { department_name: answers.departmentName }, 
                 (err, results) => {
+                    //Error handling
                     if (err) {
-                        console.error(err);
-                        return; // Stop execution on error
+                        console.error(err)
+                        return
                     }
+                    //Lets the user know a new department is added
                     console.log('New department added!')
-                    main();
+                    //Returns to the main function after the new department is added
+                    main()
                 })
             })
         }
     })
 }
-
+//Calls the main function to start the application
 main()
